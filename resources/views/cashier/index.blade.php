5c92ffae62def5f1ff04ceea34e3cf232b148e10
ahmedkhaleel <duZhuqkyptu8barnuz>
2021-05-20 00:02:23 +0200

    save payment part 1

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -42,17 +42,27 @@
                 </div>
                 <div class="modal-body">
                     <h3 class="totalAmount"></h3>
+                    <h3 class="changeAmount"></h3>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text">$</span>
                         </div>
                         <input type="number" id="received-amount" class="form-control" >
                     </div>
-                    <h3 class="changeAmount"></h3>
+
+                    <div class="form-group">
+                        <label for="payment">Payment Type</label>
+                        <select class="form-control" id="payment-type">
+                            <option value="cash">Cash</option>
+                            <option value="credit card">Credit Card</option>
+                        </select>
+                    </div>
                 </div>
+
+
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary btn-save-payment ">Save Payment</button>
+                    <button type="button" class="btn btn-primary btn-save-payment disabled ">Save Payment</button>
                 </div>
             </div>
         </div>
@@ -88,6 +98,7 @@
             });
             var SELECTED_TABLE_ID ="";
             var SELECTED_TABLE_NAME ="";
+            var SALE_ID = "";
             //DETECT BUTTON TABLE ONCLICK TO SHOW TABLE DATA
             $("#table-detail").on("click",".btn-table",function(){
                  SELECTED_TABLE_ID = $(this).data("id");
@@ -157,7 +168,8 @@
                 var totalAmount = $(this).attr('data-totalAmount');
                 $(".totalAmount").html("Total Amount  " + totalAmount );
                 $("#received-amount").val('');
-                $("#received-amount").html('');
+                $("#changeAmount").html('');
+                SALE_ID = $(this).data('id');
             });
 
             // calculate change
@@ -165,7 +177,7 @@
                 var totalAmount = $(".btn-payment").attr('data-totalAmount');
                 var receivedAmount = $(this).val();
                 var changeAmount = receivedAmount - totalAmount;
-                $(".changeAmount").html("Change Amount : $ " + changeAmount);
+                $(".changeAmount").html("Total Amount : $ " + changeAmount);
                 //check if cashier enter thme right amount ,then enable or disable save payment button
                 if(changeAmount >= 0) {
                     $('.btn-save-payment').prop('disabled',false);
@@ -174,7 +186,13 @@
                 }
 
             });
-
+            // save paymet
+            $(".btn-save-payment").click(function(){
+                var receivedAmount = $("#received-amount").val();
+                var paymentType = $("#payment-type").val();
+                var saleId = SALE_ID;
+                alert(saleId);
+            })
 
         });
     </script>
